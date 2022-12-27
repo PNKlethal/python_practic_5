20da5d03d27f485e45ede1059a185b63858b2d8c
PNKlethal <seregaa99@bk,ru>
2022-12-27 22:19:11 +0600

    Первое задание

---
+++ main_1.py
@@ -0,0 +1,14 @@
+
+my_f = open('text.txt', 'w')
+line = input('Введите текст:  \n')
+while line:
+    my_f.writelines(line)
+    line = input('Введите текст \n')
+    if not line:
+        break
+
+my_f.close()
+my_f = open('text.txt', 'r')
+content = my_f.readlines()
+print(content)
+my_f.close()
